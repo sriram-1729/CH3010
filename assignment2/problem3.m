3329723f4e3ade1d56738f260b28787262230741
Sriram <kinjarapusriram2000@gmail.com>
2020-08-21 12:55:27 +0530

    Edited assignment2/problem3.m so that only two data points are used to find the best polynomial fit.

--- assignment2/problem3.m
+++ assignment2/problem3.m
@@ -1,8 +1,8 @@
-t = [0, 8, 100];
-C_a = [0.5, 0.3335, 0.1665];
+t = [0, 8];
+C_a = [0.5, 0.3335];
 
 log_term = log(0.499*0.5./(1.499.*C_a-0.499*0.5));
-x = 0:1:100;
+x = 0:0.1:8;
 p = polyfit(t, log_term, 1);
 y = polyval(p, x);
 
@@ -11,5 +11,5 @@ hold on;
 title('ln($\frac{0.499C_{A0}}{1.499C_{A}-0.499C}$) vs t','interpreter','latex');
 xlabel('t (min)');
 ylabel('ln($\frac{0.499C_{A0}}{1.499C_{A}-0.499C}$)', 'interpreter','latex');
-legend('Given data', 'ln($\frac{0.499C_{A0}}{1.499C_{A}-0.499C}$) = 0.087t', 'interpreter','latex');
+legend('Given data', 'ln($\frac{0.499C_{A0}}{1.499C_{A}-0.499C}$) = 0.086t', 'interpreter','latex');
 hold off;
